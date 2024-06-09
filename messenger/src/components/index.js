64ba9a650c71e2e9c1dbcf677899bab1daa98893
arishkaa05 <pokazatskaya02@mail.ru>
2024-06-09 13:13:40 +0300

    login-psge, express-server

---
+++ messenger/src/components/index.js
@@ -0,0 +1,8 @@
+export { Button } from './button';
+export { Input } from './input'; 
+export { Link } from './link';
+export { PageTitle } from './page-title';
+export { Dialog } from './dialog';
+export { InputField } from './input-field';
+
+console.log(35)
