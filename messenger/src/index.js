6dc5f878338577dedd18b43cbd6e5b55692df3d5
arishkaa05 <pokazatskaya02@mail.ru>
2024-06-13 03:04:20 +0300

    pages, validation

--- messenger/src/index.js
+++ messenger/src/index.js
@@ -4,21 +4,38 @@ import * as Components from './components';
 import * as Pages from './pages';
 
 const pages = {
-  'chat': [ Pages.ChatPage ],
+  'signin': [ Pages.SigninPage ],
   'login': [ Pages.LoginPage ],
-};
+  'chat': [ Pages.ChatPage ],
+  'notFound': [ Pages.NotFoundPage ],
+  'fix': [ Pages.FixPage ],
+  'profile': [ Pages.ProfilePage ],
+  'password': [ Pages.PasswordPage ]
+}; 
 
 Object.entries(Components).forEach(([ name, component ]) => {
   Handlebars.registerPartial(name, component);
 });
 
-function navigate(page) {
-  const [ source, args ] = pages[page];
-  const handlebarsFunct = Handlebars.compile(source);
-  document.body.innerHTML = handlebarsFunct(args);
+function navigate(page) { 
+  if (pages.hasOwnProperty(page)) {
+    const [ source, args ] = pages[page];
+    const handlebarsFunct = Handlebars.compile(source);
+    document.body.innerHTML = handlebarsFunct(args);
+
+    const url = new URL(window.location.href);
+    url.pathname = `/${page}`;
+    window.history.pushState({}, '', url.toString());
+  } else {
+    navigate('notFound'); 
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => navigate('login'));
+document.addEventListener('DOMContentLoaded', () => {
+  const url = new URL(window.location.href);
+  const path = url.pathname.slice(1) || 'login'; // Получаем путь из URL-адреса
+  navigate(path);
+});
 
 document.addEventListener('click', e => {
   const page = e.target.getAttribute('page');
