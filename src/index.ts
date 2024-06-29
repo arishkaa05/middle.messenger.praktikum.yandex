74786dc94dc994ea08e649f90bfd88f1b50ed021
arishkaa05 <pokazatskaya02@mail.ru>
2024-06-30 00:50:26 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
-import Handlebars from 'handlebars';
-import * as Components from './components/index';
-import * as Blocks from './blocks/index';
 import navigate from './modules/navigate';
-
-Object.entries(Components).forEach(([name, component]) => {
-    Handlebars.registerPartial(name, component);
-});
-
-Object.entries(Blocks).forEach(([name, block]) => {
-    Handlebars.registerPartial(name, block);
-});
 
 document.addEventListener('DOMContentLoaded', () => {
     const url = new URL(window.location.href);
--- src/pages/login-page/index.ts
+++ src/pages/login-page/index.ts
@@ -1,4 +1,2 @@
-import './login-page.scss';
-
 export { createLoginPage } from './module';
 export { default as LoginPage } from './login-page.hbs?raw';
