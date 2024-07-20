abadf8615154a6ae0b0e9f72941ce361b0d0f78b
arishkaa05 <pokazatskaya02@mail.ru>
2024-07-20 17:36:07 +0300

    fix

--- src/blocks/message-container/module.ts
+++ src/blocks/message-container/module.ts
@@ -1,68 +1,102 @@
-import { UserMessageModule } from '../../components/user-message/module';
-import Block from '../../modules/Block';
-import store from '../../modules/Store';
-import { IMessageContainer } from '../../modules/types';
-import { getChatToken } from '../../pages/chat-page/chat.services';
-import { MessageContainer } from './index';
+import { UserMessageModule } from "../../components/user-message/module";
+import Block from "../../modules/Block";
+import store from "../../modules/Store";
+import { IMessageContainer } from "../../modules/types";
+import { getChatToken } from "../../pages/chat-page/chat.services";
+import { UserMessageListModule } from "../user-message-list/module";
+import { MessageContainer } from "./index";
 
 export class MessageContainerModule extends Block {
-    constructor(props: IMessageContainer) {
-        super(props);
-        this.props.messages = props.messages;
-    }
+  constructor(props: IMessageContainer) {
+    super(props);
+    this.props.messages = props.messages;
+  }
 
-    render() {
-        return this.makeFragment(MessageContainer, this.props);
-    }
+  render() {
+    return this.makeFragment(MessageContainer, this.props);
+  }
 }
 
 export const openMessageContainer = async () => {
-    const chatId = store.getState().activeChat.id;
-    const userId = store.getState().userData.id;
-    // console.log(chatId);
+  const chatId = store.getState().activeChat.id;
+  const userId = store.getState().userData.id;
+  console.log(chatId);
 
-    // store.dispatch({ type: "SET_NEW_MSG", messageList: [
+  store.dispatch({
+    type: "SET_NEW_MSG",
+    messageList: [
+      new UserMessageModule({
+        id: 3,
+        isOwn: false,
+        message: "Привет! Как дела?",
+        time: "10:43",
+        count: 2,
+      }),
+    ],
+  });
+  const token = await getChatToken(chatId);
+  const socket = new WebSocket(`wss://ya-praktikum.tech/ws/chats/${userId}/${chatId}/${token.token}`);
+  socket.addEventListener("open", () => {
+    console.log("Соединение установлено");
 
-    //   new UserMessageModule(
-    //     {
-    //     id: 3,
-    //     isOwn: false,
-    //     message: "Привет! Как дела?",
-    //     time: "10:43",
-    //     count: 2,
-    //   }
-    //   )
-    //   ], });
-    //   const token = await getChatToken(chatId);
-    //   const socket = new WebSocket(`wss://ya-praktikum.tech/ws/chats/${userId}/${chatId}/${token.token}`);
-    //   socket.addEventListener("open", () => {
-    //     console.log("Соединение установлено");
+    socket.send(
+      JSON.stringify({
+        content: "Моё первое сообщение миру!",
+        type: "message",
+      })
+    );
+  });
 
-    //     socket.send(
-    //       JSON.stringify({
-    //         content: "Моё первое сообщение миру!",
-    //         type: "message",
-    //       })
-    //     );
-    //   });
+  socket.addEventListener("close", (event) => {
+    if (event.wasClean) {
+      console.log("Соединение закрыто чисто");
+    } else {
+      console.log("Обрыв соединения");
+    }
 
-    //   socket.addEventListener("close", (event) => {
-    //     if (event.wasClean) {
-    //       console.log("Соединение закрыто чисто");
-    //     } else {
-    //       console.log("Обрыв соединения");
-    //     }
+    console.log(`Код: ${event.code} | Причина: ${event.reason}`);
+  });
 
-    //     console.log(`Код: ${event.code} | Причина: ${event.reason}`);
-    //   });
+  socket.addEventListener("message", (event) => {
+    // console.log(event);
+    // console.log("Получены данные", event.data);
+    try {
+      const messageData = JSON.parse(event.data);
+    //   console.log(store.getState().userMessagesList);
+      if (!store.getState().userMessagesList) {
+        // const userMessagesList = new UserMessageListModule({
+        //   messages: [
+        //     new UserMessageModule({
+        //       id: messageData.id,
+        //       isOwn: false,
+        //       message: messageData.content,
+        //       time: messageData.time,
+        //     }),
+        //   ],
+        // });
+        // console.log(userMessagesList)
+        const test = new UserMessageListModule({
+            messages: [
+              new UserMessageModule({
+                id: 3,
+                isOwn: false,
+                message: "dак dgла?",
+                time: "10:43",
+                count: 2,
+              }),
+            ],
+          })
+        // store.dispatch({ type: "SET_NEW_MSG", userMessagesList: test });
+        console.log(store.getState().userMessagesList);
+      }
+    } catch (error) {
+      console.error("Ошибка при парсинге JSON:", error);
+    }
+  });
 
-    //   socket.addEventListener("message", (event) => {
-    //     console.log("Получены данные", event.data);
-    //   });
+  socket.addEventListener("error", (event: any) => {
+    console.log("Ошибка", event.message);
+  });
 
-    //   socket.addEventListener("error", (event: any) => {
-    //     console.log("Ошибка", event.message);
-    //   });
-
-//   console.log(chatId, userId, token);
+  console.log(chatId, userId, token);
 };
--- src/modules/Hoc.ts
+++ src/modules/Hoc.ts
@@ -1,21 +1,21 @@
-import Block from './Block';
-import store from './Store';
-import isEqual from '../helpers/isEqual';
+import Block from "./Block";
+import store from "./Store";
+import isEqual from "../helpers/isEqual";
 
 export function connect(Component: typeof Block, mapStateToProps: (state: any) => any) {
-    return class extends Component {
-        constructor(props: any) {
-            let state = mapStateToProps(store.getState());
-            super({ ...props, ...state });
+  return class extends Component {
+    constructor(props: any) {
+      let state = mapStateToProps(store.getState());
+      super({ ...props, ...state });
 
-            store.subscribe(() => {
-                const newState = mapStateToProps(store.getState());
+      store.subscribe(() => {
+        const newState = mapStateToProps(store.getState());
 
-                if (!isEqual(state, newState)) {
-                    this.setProps({ ...newState });
-                }
-                state = newState;
-            });
+        if (!isEqual(state, newState)) {
+          this.setProps({ ...newState });
         }
-    };
+        state = newState;
+      });
+    }
+  };
 }
